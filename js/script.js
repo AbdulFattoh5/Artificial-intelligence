40a07e8fd13991fea4c7a715603388c746924543
AbdulFattoh5 <abdulfattokh55@gmail.com>
2022-12-01 15:29:35 +0500

    header

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// burger menu
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('burger').addEventListener('click', function(){
+        document.querySelector('header').classList.toggle('open')
+    })
+})
+
