09678e8925f68d088a509e00646cfd333503239f
AbdulFattoh5 <abdulfattokh55@gmail.com>
2023-05-24 12:50:19 +0500

    slider

--- js/script.js
+++ js/script.js
@@ -4,4 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('header').classList.toggle('open')
     })
 })
-// Swiper
+// Carousel
+const gap = 1;
+
+const carousel = document.getElementById("carousel_part"),
+    content = document.getElementById("carousel_content"),
+    next = document.getElementById("next"),
+    prev = document.getElementById("prev");
+
+let width = carousel.offsetWidth;
+
+next.addEventListener("click", e => {
+    carousel.scrollBy(width + gap, 0);
+    if (carousel.scrollWidth !== 0) {
+        prev.style.display = "flex";
+    }
+    if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "none";
+    }
+});
+prev.addEventListener("click", e => {
+    carousel.scrollBy(-(width + gap), 0);
+    if (carousel.scrollLeft - width - gap <= 0) {
+        prev.style.display = "none";
+    }
+    if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+        next.style.display = "flex";
+    }
+});
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
